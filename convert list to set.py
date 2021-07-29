0b6a8c5366429ef7524c4314cedd1667bd2a1dcc
KalpanaRedddy <88103065+KalpanaRedddy@users.noreply.github.com>
2021-07-29 09:51:11 +0530

    Update convert list to set.py

--- convert list to set.py
+++ convert list to set.py
@@ -1,4 +1,4 @@
 names_list=['kalpana','nani','manasa','sumith','pavi','mythri','latha']
-print(f'original list:\n{list_animal}\n')
+print(f'original list:\n{names_list}\n')
 names_set = set(list_animal)
 print(f'set from list:\n{names_set}')
